677ec6b59a9ce3483d0572bcf54f2a02bec4efe5
Oleg Volcov <bojikch15@gmail.com>
2021-11-05 18:33:14 +0200

    Modified `UseCase` to have functional to send `Message` in db.
     + Added possibility to insert data about chats on `SignUp`.

--- pkg/auth/usecase/usecase.go
+++ pkg/auth/usecase/usecase.go
@@ -6,21 +6,27 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/auth"
+	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/auth/repository/mongo"
 	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/models"
 	"time"
 )
 
 type Authorizer struct {
-	repo auth.Repository
+	userRepo    auth.Repository
+	chatRepo    auth.Repository
+	messageRepo *mongo.MessageRepository
 
 	hashSalt       string
 	signingKey     []byte
 	expireDuration time.Duration
 }
 
-func NewAuthorizer(repo auth.Repository, hashSalt string, signingKey []byte, expireDuration time.Duration) *Authorizer {
+func NewAuthorizer(userRepo auth.Repository, chatRepo auth.Repository, messageRepo *mongo.MessageRepository,
+	hashSalt string, signingKey []byte, expireDuration time.Duration) *Authorizer {
 	return &Authorizer{
-		repo:           repo,
+		userRepo:       userRepo,
+		chatRepo:       chatRepo,
+		messageRepo:    messageRepo,
 		hashSalt:       hashSalt,
 		signingKey:     signingKey,
 		expireDuration: expireDuration,
@@ -33,7 +39,13 @@ func (a *Authorizer) SignUp(ctx context.Context, user *models.User) error {
 	pwd.Write([]byte(a.hashSalt))
 	user.Password = fmt.Sprintf("%x", pwd.Sum(nil))
 
-	return a.repo.Insert(ctx, user)
+	result := a.userRepo.Insert(ctx, user)
+	if result != nil {
+		return result
+	}
+
+	_ = a.chatRepo.Insert(ctx, user)
+	return result
 }
 
 func (a *Authorizer) SignIn(ctx context.Context, user *models.User) (string, error) {
@@ -42,7 +54,7 @@ func (a *Authorizer) SignIn(ctx context.Context, user *models.User) (string, err
 	pwd.Write([]byte(a.hashSalt))
 	user.Password = fmt.Sprintf("%x", pwd.Sum(nil))
 
-	user, err := a.repo.Get(ctx, user.Username, user.Password)
+	user, err := a.userRepo.Get(ctx, user.Username, user.Password)
 	if err != nil {
 		return "", err
 	}
@@ -56,4 +68,13 @@ func (a *Authorizer) SignIn(ctx context.Context, user *models.User) (string, err
 	})
 
 	return token.SignedString(a.signingKey)
+}
+
+func (a *Authorizer) Send(ctx context.Context, message *models.Message) error {
+	result := a.messageRepo.Send(ctx, message)
+	if result != nil {
+		return result
+	}
+
+	return result
 }
