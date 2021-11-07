8888565290230a605282b7ac91028eae1bd7cf51
Oleg Volcov <bojikch15@gmail.com>
2021-11-07 17:57:53 +0200

    Modified `register` module for future new group `message`.

--- pkg/auth/delivery/register.go
+++ pkg/auth/delivery/register.go
@@ -5,9 +5,15 @@ import (
 	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/auth"
 )
 
-func RegisterHTTPEndpoints(router *gin.RouterGroup, usecase auth.UseCase) {
+func RegisterHTTPAuthEndpoints(router *gin.RouterGroup, usecase auth.UseCase) {
 	h := newHandler(usecase)
 
 	router.POST("/sign-up", h.signUp)
 	router.POST("/sign-in", h.signIn)
+}
+
+func RegisterHTTPMessageEndpoints(router *gin.RouterGroup, usecase auth.UseCase) {
+	h := newHandler(usecase)
+
+	router.POST("/send", h.send)
 }
