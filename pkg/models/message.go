15fc6eded0173f1fd9c69b14f6ae2fa91f2fc5bb
Volcov Oleg <bojikch15@gmail.com>
2022-01-15 13:38:48 +0200

    Modified `Message` structure to contain date of creation.

--- pkg/models/message.go
+++ pkg/models/message.go
@@ -1,10 +1,14 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type Message struct {
 	ID            primitive.ObjectID `json:"id" bson:"_id"`
 	AuthorID      string             `json:"author_id" bson:"author_id"`
 	DestinationID string             `json:"destination_id" bson:"destination_id"`
 	Content       string             `json:"content" bson:"content"`
+	Date          time.Time          `bson:"date"`
 }
