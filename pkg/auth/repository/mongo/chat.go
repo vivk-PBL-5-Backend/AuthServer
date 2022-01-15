831beb259b8d413dcc5415fc28ff3910aed30296
Volcov Oleg <bojikch15@gmail.com>
2022-01-15 15:13:01 +0200

    Added functions for `Chat` content.

--- pkg/auth/repository/mongo/chat.go
+++ pkg/auth/repository/mongo/chat.go
@@ -5,7 +5,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/auth"
 	"github.com/vivk-PBL-5-Backend/AuthServer/pkg/models"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,20 +19,77 @@ func NewChatRepository(db *mongo.Database, collection string) *ChatRepository {
 	}
 }
 
-func (r *ChatRepository) Insert(ctx context.Context, user *models.User) error {
-	chat := new(models.Chat)
-	chat.Username = user.Username
-	chat.Messages = make([]primitive.ObjectID, 0)
-
-	_, err := r.db.InsertOne(ctx, chat)
+func (r *ChatRepository) AddCompanion(ctx context.Context, userID string, companionID string) error {
+	chat, err := r.findOrCreate(ctx, userID)
 	if err != nil {
-		log.Errorf("error on inserting user: %s", err.Error())
+		return err
+	}
+
+	chat.Companions = append(chat.Companions, companionID)
+	if _, err = r.db.UpdateOne(ctx, bson.D{{"_id", chat.Username}}, bson.M{"$set": chat}); err != nil {
+		log.Errorf("error on inserting companion in chat: %s", err.Error())
 		return auth.ErrUserAlreadyExists
 	}
 
 	return nil
 }
 
-func (r *ChatRepository) Get(ctx context.Context, username, password string) (*models.User, error) {
-	return nil, auth.ErrUserDoesNotExist
+func (r *ChatRepository) RemoveCompanion(ctx context.Context, userID string, companionID string) error {
+	chat, err := r.findOrCreate(ctx, userID)
+	if err != nil {
+		return err
+	}
+
+	companionIndex := -1
+	for i, elem := range chat.Companions {
+		if elem == companionID {
+			companionIndex = i
+			break
+		}
+	}
+
+	if companionIndex == -1 {
+		return nil
+	}
+
+	chat.Companions[companionIndex] = chat.Companions[len(chat.Companions)-1]
+	chat.Companions = chat.Companions[:len(chat.Companions)-1]
+
+	if _, err = r.db.UpdateOne(ctx, bson.D{{"_id", chat.Username}}, bson.M{"$set": chat}); err != nil {
+		log.Errorf("error on inserting companion in chat: %s", err.Error())
+		return auth.ErrUserAlreadyExists
+	}
+
+	return nil
+}
+
+func (r *ChatRepository) GetCompanions(ctx context.Context, userID string) ([]string, error) {
+	chat, err := r.findOrCreate(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return chat.Companions, nil
+}
+
+func (r *ChatRepository) findOrCreate(ctx context.Context, userID string) (*models.Chat, error) {
+	chat := new(models.Chat)
+
+	if err := r.db.FindOne(ctx, bson.M{"_id": userID}).Decode(chat); err != nil {
+		if err != mongo.ErrNoDocuments {
+			log.Errorf("error occured while getting chat from db: %s", err.Error())
+			return nil, err
+		}
+
+		chat.Username = userID
+		chat.Companions = make([]string, 0)
+
+		_, err = r.db.InsertOne(ctx, chat)
+		if err != nil {
+			log.Errorf("error on inserting chat: %s", err.Error())
+			return nil, auth.ErrUserAlreadyExists
+		}
+	}
+
+	return chat, nil
 }
