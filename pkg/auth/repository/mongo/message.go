9a9b68dbd735bbbb02ac59b324d8d54922a1a25b
Volcov Oleg <bojikch15@gmail.com>
2022-01-15 13:42:20 +0200

    Modified old `Message` sending behaviour.

--- pkg/auth/repository/mongo/message.go
+++ pkg/auth/repository/mongo/message.go
@@ -22,26 +22,9 @@ func NewMessageRepository(db *mongo.Database, chatCollection string, messageColl
 }
 
 func (r *MessageRepository) Send(ctx context.Context, message *models.Message) error {
-	chat := new(models.Chat)
-
 	_, err := r.messageDB.InsertOne(ctx, message)
 	if err != nil {
 		log.Errorf("error on inserting message: %s", err.Error())
-		return auth.ErrUserAlreadyExists
-	}
-
-	if err = r.chatDB.FindOne(ctx, bson.M{"_id": message.DestinationID}).Decode(chat); err != nil {
-		log.Errorf("error occured while getting chat from db: %s", err.Error())
-		if err == mongo.ErrNoDocuments {
-			return auth.ErrUserDoesNotExist
-		}
-
-		return err
-	}
-
-	chat.Messages = append(chat.Messages, message.ID)
-	if _, err = r.chatDB.UpdateOne(ctx, bson.D{{"_id", message.DestinationID}}, bson.M{"$set": chat}); err != nil {
-		log.Errorf("error on inserting message in chat: %s", err.Error())
 		return auth.ErrUserAlreadyExists
 	}
 
