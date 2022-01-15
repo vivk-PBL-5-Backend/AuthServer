ab416541b7461912dd6842e0f8379c8ed82ca6c1
Volcov Oleg <bojikch15@gmail.com>
2022-01-15 13:41:15 +0200

    Created behaviour for getting all messages from chat.

--- pkg/auth/repository/mongo/message.go
+++ pkg/auth/repository/mongo/message.go
@@ -47,3 +47,43 @@ func (r *MessageRepository) Send(ctx context.Context, message *models.Message) e
 
 	return nil
 }
+
+func (r *MessageRepository) Get(ctx context.Context, destination string, author string) ([]models.Message, error) {
+	messages := make([]models.Message, 0)
+
+	cursor, err := r.messageDB.Find(ctx, bson.M{"destination_id": destination, "author_id": author})
+	if err == nil {
+		for cursor.Next(ctx) {
+			var elem models.Message
+			err = cursor.Decode(&elem)
+			if err != nil {
+				log.Errorf("error on get message: %s", err.Error())
+				return nil, auth.ErrUserAlreadyExists
+			}
+
+			messages = append(messages, elem)
+		}
+	}
+
+	cursor, err = r.messageDB.Find(ctx, bson.M{"destination_id": author, "author_id": destination})
+	if err == nil {
+		for cursor.Next(ctx) {
+			var elem models.Message
+			err = cursor.Decode(&elem)
+			if err != nil {
+				log.Errorf("error on get message: %s", err.Error())
+				return nil, auth.ErrUserAlreadyExists
+			}
+
+			_, err = r.messageDB.DeleteOne(ctx, bson.M{"_id": elem.ID})
+			if err != nil {
+				log.Errorf("error on get message: %s", err.Error())
+				return nil, auth.ErrUserAlreadyExists
+			}
+
+			messages = append(messages, elem)
+		}
+	}
+
+	return messages, nil
+}
