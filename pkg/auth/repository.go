88ff05dae168f7fa8b9dbe3564da5608c277a990
Volcov Oleg <bojikch15@gmail.com>
2022-01-18 08:33:09 +0200

    Refactor `chat` rout and `add component` functional to check if user exist.

--- pkg/auth/repository.go
+++ pkg/auth/repository.go
@@ -8,4 +8,5 @@ import (
 type Repository interface {
 	Insert(ctx context.Context, user *models.User) error
 	Get(ctx context.Context, username, password string) (*models.User, error)
+	Exist(ctx context.Context, userID string) error
 }
